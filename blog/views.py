8e4f0a75ae656a41f0733c3b136c06736eb7cd7b
bibhahs iman <imanilbag>
2017-04-19 23:23:38 +0430

    second edit

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import (render, get_object_or_404, redirect)
 from django.views.generic import View
 from .models import Post
+from .forms import PostForm
 
 from django.views.decorators.http import require_http_methods
 
@@ -8,13 +9,29 @@ from django.views.decorators.http import require_http_methods
 def post_list(request):
 	return render(request, 'blog/post_list.html', {'post_list': post.objects.all()}) 
 @require_http_methods(['HEAD', 'GET'])
+
 def post_detail(request, year, month, slug):
 	post =get_object_or_404(Post, pub_date__year=year, pub_date__month=month, slug=slug)
 	return render(request, 'blog/post_detail.html', {'post':post})
-# Create your views here.
+
 class PostList (View):
 	
 	template_name = 'blog/post_list.html'
 	
 	def get (self, request):
 		return render(request, self.template_name, {'post_list': Post.objects.all()})
+
+class PostCreate (View):
+	form_class = PostForm
+	template_name = 'blog/post_form.html'
+
+	def get(self, request):
+		return render(request, self.template_name, {'form':self.form_class})
+
+	def post(self, request):
+		bound_form = self.form_class(request.POST)
+		if bound_form.is_valid():
+			new_post = bound_form.save()
+			return redirect(new_post)
+		else:
+			return render(request, self.template_name, {'form':bound_form})
